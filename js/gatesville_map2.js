c30f97c816cbf0f8a5c05976fb06458a260b1873
James Markham Desktop <jmarkham@unm.edu>
2018-02-07 12:27:43 -0700

    testing

---
+++ js/gatesville_map2.js
@@ -0,0 +1,11 @@
+function initialize() {
+
+<script>
+   		var map;
+      	function initMap() {
+        	map = new google.maps.Map(document.getElementById('map_canvas'), {
+            	center: {lat: 31.4351600, lng: -97.7439100},
+          		zoom: 15
+        	});
+      	}
+  	  </script>
